76355398a4789622cba57502012d08f5966acd84
MaZderMind <github@mazdermind.de>
2015-03-10 13:00:58 +0100

    more path-fixes

--- upload-fossgis.sh
+++ upload-fossgis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "uploading mp4s"
-./upload.py --schedule http://www.fossgis.de/konferenz/2015/programm/schedule.de.xml --recordings /video/FOSSGIS14/ --extension .mp4 --finished /video/FOSSGIS15/uploaded/ --auphonic-login ./login --auphonic-preset PgeXpchzojrrgM25vVPw3U
+./upload.py --schedule http://www.fossgis.de/konferenz/2015/programm/schedule.de.xml --recordings /video/FOSSGIS15/encoded/ --extension .mp4 --finished /video/FOSSGIS15/uploaded/ --auphonic-login ./login --auphonic-preset PgeXpchzojrrgM25vVPw3U
 
 echo "uploading webms"
-./upload.py --schedule http://www.fossgis.de/konferenz/2015/programm/schedule.de.xml --recordings /video/FOSSGIS14/ --extension .webm --finished /video/FOSSGIS15/uploaded/ --auphonic-login ./login --auphonic-preset hqvsm8SCL3QmCYkCDhbm7T
+./upload.py --schedule http://www.fossgis.de/konferenz/2015/programm/schedule.de.xml --recordings /video/FOSSGIS15/encoded/ --extension .webm --finished /video/FOSSGIS15/uploaded/ --auphonic-login ./login --auphonic-preset hqvsm8SCL3QmCYkCDhbm7T
